81326c053eead05efa2519c45cbb329f898ca6d1
Ruben <rubnitos@hotmail.com>
2023-06-02 18:33:55 +0100

    redux working on ep

--- src/components/Series/Episode.tsx
+++ src/components/Series/Episode.tsx
@@ -3,19 +3,33 @@ import PropTypes from "prop-types";
 import { getSeriesSeason } from "../../actions/series";
 import { connect } from "react-redux";
 
-const Episode = ({ season }: { season: any }) => {
+const Episode = ({
+  season,
+  getSeriesSeason,
+}: {
+  season: any;
+  getSeriesSeason: any;
+}) => {
   const id = window.location.pathname.split("/")[2];
   const seasonNr = window.location.pathname.split("/")[4];
+  const episodeNr = window.location.pathname.split("/")[6];
 
   useEffect(() => {
     getSeriesSeason(Number(id), Number(seasonNr));
-  });
+  }, []);
 
-  return <div>Episode</div>;
+  if (!season) return <p>Loading</p>;
+
+  return (
+    <div>
+      <img src={"https://image.tmdb.org/t/p/w500/" + season.poster_path}></img>
+    </div>
+  );
 };
 
 Episode.propTypes = {
   getSeriesSeason: PropTypes.func.isRequired,
+  series: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
